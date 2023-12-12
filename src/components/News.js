397a69a16dac52adfba9e259d986dab541882004
Anshul <a20110100680@gmail.com>
2023-12-12 18:08:13 +0530

    next & prev button/ disable for no next and prev page

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
-{/* we have used class based component to iterate through the news, the moment the class is loaded constuctor will be exicuted,
-so we have defined states(aritcles, loading) as object inside constructor  */}
+/* we have used class based component to iterate through the news, the moment the class is loaded constuctor will be exicuted,
+so we have defined states(aritcles, loading) as object inside constructor  */
 
 export default class News extends Component {
 
@@ -12,36 +12,60 @@ export default class News extends Component {
                             super();
                             console.log("Hello i am contructor from news");
 
-                            {/*State as Object*/}
+                            /*State as Object*/
                             this.state = {
                                       articles: [],
-                                      loading: false
+                                      loading: false,
+                                      page: 1
                             }
                 }
     /*Fetch data from new API using using fetch */
                 async componentDidMount() {
-                    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9b33c920ed65479ebf547d39acf49c90";
+                    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9b33c920ed65479ebf547d39acf49c90&page=1pageSize=20";
                     let data = await fetch(url);
                     let parsedData = await data.json();
                     console.log(parsedData);
-                    const filteredArticles = parsedData.articles.filter(article => article.title && article.urlToImage);
-
-    if (filteredArticles.length > 0) {
-      this.setState({ articles: filteredArticles });
-    } else {
-      this.setState({ articles: [], error: "No news available" });
-      // You can also handle the absence of news by displaying a default message or fallback content
-    }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    // Handle errors here
-    this.setState({ articles: [], error: "Error fetching news data" });
+                    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
                 }
+                //Next page button
+                 handlePrevClick = async() => {
+   
+                   // console.log("previous");
+                    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=9b33c920ed65479ebf547d39acf49c90&page=${this.state.page-1}&pageSize=20`;
+                    let data = await fetch(url);
+                    let parsedData = await data.json();
+                    console.log(parsedData);
+                    this.setState({
+                      articles: parsedData.articles,
+                      page: this.state.page-1
+                    })
+                }
+
+                //Previous page button
+                handleNextClick = async() => {
+
+
+                  if(this.state.page + 1  > Math.ceil(this.state.totalResults/20)){
+                    //do noting 
+                  } 
+                  else {
+                    // console.log("next");
+                    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=9b33c920ed65479ebf547d39acf49c90&page=${this.state.page+1}&pageSize=20`;
+                    let data = await fetch(url);
+                    let parsedData = await data.json();
+                    console.log(parsedData);
+                    this.setState({
+                      articles: parsedData.articles,
+                      page: this.state.page+1
+                    })
+                  }
+                }
+
 
   render() {
     return (
-      <div className='container my-3'>
-        <h2>NewsMonkey Top - Headlines</h2>
+      <div className='container my-5'>
+        <h1>NewsMonkey Top - Headlines</h1>
 
          <div className="row">
             {/*itrating through all the news articles using map*/}
@@ -52,7 +76,11 @@ export default class News extends Component {
                                       imageUrl={element.urlToImage} newsUrl={element.url} />
                          </div>
             })}
-          </div> 
+          </div>  
+                <div className="container d-flex justify-content-between">
+                       <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Prev</button>
+                       <button type="button" className ="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
       </div>
     )
   }
